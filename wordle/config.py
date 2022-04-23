416ebb844797f5f43bf5732b5586448cd56de0e2
Marco Tinacci <tinacci.marco@gmail.com>
2022-04-23 17:35:26 +0100

    added env vars

--- wordle/config.py
+++ wordle/config.py
@@ -1,7 +1,13 @@
+import os
+import logging
 from typing import Final
-
+from pathlib import Path
 
 SYMBOL_MATCH: Final = "X"
-SYMBOL_MISPLACED: Final = "?"
+SYMBOL_MISPLACED: Final = "."
 SYMBOL_MISS: Final = "_"
 MAX_ATTEMPTS: Final = 6
+
+DATA_ROOT = Path(__file__).parent.parent / "data"
+DEBUG = os.environ.get("DEBUG", False)
+LOG_LEVEL = logging.DEBUG if DEBUG else logging.WARNING
